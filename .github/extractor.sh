0eb0c8d6c339a548b0bac90c98bf95bcf8dbb830
robalb <11277482+robalb@users.noreply.github.com>
2023-09-03 14:10:21 +0200

    fixed typo

--- .github/extractor.sh
+++ .github/extractor.sh
@@ -26,7 +26,7 @@ cat << EOF > Dockerfile
 EOF
 
 for image in $images; do
-    echo "found": $image"
+    echo "found: $image"
     echo "FROM $image" >> Dockerfile
     echo "RUN echo $image" >> Dockerfile
     echo " " >> Dockerfile
